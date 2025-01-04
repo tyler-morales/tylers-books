3c073d8b5184526014d095bd42156b305464ec05
tyler-morales <tylermorales.dev@gmail.com>
2025-01-03 21:53:16 -0600

    Update env config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 const nextConfig = {
   output: "export",
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  basePath: process.env.NODE_ENV === "production" ? "/tylers-books" : "",
   trailingSlash: true,
+  env: {
+    NEXT_PUBLIC_API_BASE_PATH: process.env.NEXT_PUBLIC_API_BASE_PATH,
+  },
 };
 
 module.exports = nextConfig;
