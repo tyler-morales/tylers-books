0c1c17e66ab9bbdeeb088d7212e796bd138bffea
tyler-morales <tylermorales.dev@gmail.com>
2025-01-03 21:52:13 -0600

    Added gradient background

--- app/layout.js
+++ app/layout.js
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <main>{children}</main>
+        <main className="bg-gradient-to-t from-slate-900 h-screen">{children}</main>
       </body>
     </html>
   );
