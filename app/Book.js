c4e682bb2996f8283c88afcff0a6608293dc1b50
tyler-morales <tylermorales.dev@gmail.com>
2025-01-23 11:30:47 -0600

    Increase random bounce initial height

--- app/Book.js
+++ app/Book.js
@@ -82,7 +82,7 @@ export default function Book({
 
     return className;
   }
-  const [randomY] = useState(() => Math.floor(Math.random() * 500) + 500);
+  const [randomY] = useState(() => Math.floor(Math.random() * 500) + 800);
   const [randomBounce] = useState(() => Math.random() * (1.2 - 0.5) + 0.5); // Random between 0.5 and 1.5
   return (
     <motion.li
